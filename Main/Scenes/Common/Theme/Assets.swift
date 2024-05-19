1dca9ca8c0b246a1674913809763c1c4dac73246
Daniel Ribas <ds_ribas@hotmail.com>
2024-05-19 12:36:07 -0300

    chore: add new icons

--- Main/Scenes/Common/Theme/Assets.swift
+++ Main/Scenes/Common/Theme/Assets.swift
@@ -7,6 +7,14 @@ extension UIImage {
     case icArrowRight = "arrow.right"
     case icChevronLeft = "chevron.left"
     case icEyeHidden = "eye.hidden"
+    case icArrowDownIn = "arrow.down.in"
+    case icArrowUpOut = "arrow.up.out"
+    case icArrowReturn = "arrow.return"
+    case icBarcode = "barcode"
+    case icPercentage = "percentage"
+    case icShare = "share"
+    case icSignOut = "sign.out"
+
   }
   
   convenience init?(named: CoraImages) {
